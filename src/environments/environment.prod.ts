5ced0179690b78d7e34d0da23513b1fac2899c69
suprem <suprem@Suprem.connect>
2018-08-12 23:19:34 +0100

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: true,
+
+    region: 'us-east-2',
+
+    identityPoolId: 'us-east-2:06da5ff3-2127-48fb-a2b3-bd50d9222d5f',
+    userPoolId: 'us-east-2_rPgPrnZfO',
+    clientId: '4fanmclc1kn7horlat6bjv4hu8',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-2',
+
+    ddbTableName: 'LoginTrailpenny2',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+};
+
